f2856e15dccd9c98831921fb5ece14df3817abdc
moon9ua <moon9ua@gmail.com>
2025-01-02 18:59:33 +0900

    draft: 벽을 활용한 스노우볼 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 import Canvas from "@/components/Canvas";
+import Image from "next/image";
+import crystal from "../public/png/snow-ball.png";
 
 export default function Page() {
   return (
     <>
+      <Image
+        alt="crystal"
+        src={crystal}
+        width={350}
+        height={450}
+        id="crystal"
+      />
       <Canvas />
     </>
   );
